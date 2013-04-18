8b326692ca798ffef35c6b707a1ed2cb67973dde
Christopher Martin <ch.martin@gmail.com>
2013-04-18 17:51:02 -0300

    That was embarrassing.

--- top-z.js
+++ top-z.js
@@ -1,13 +1,13 @@
 function topZ() {
-    var topZ = 0;
+    var max = 0;
     $('body>*').each(function(el) {
         var z = el.zIndex();
         if (z === 0) {
             el.zIndex(0);
         } else {
-            topZ = Math.max(topZ, z);
+            max = Math.max(max, z);
         }
     });
-    return topZ + 1;
+    return max + 1;
 }
 
